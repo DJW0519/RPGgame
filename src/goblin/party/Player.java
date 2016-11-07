0ba90902cb68313ab808aae06d8e83af73768efa
Kristen Hanslik <kristen.hanslik@colorado.edu>
2016-11-06 18:01:43 -0700

    Added Player Class

---
+++ src/goblin/party/Player.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goblin.party;
+
+/**
+ *
+ * @author kristen
+ */
+
+
+public class Player extends Charachter {
+    private String outfitColor;
+    private String hairColor;
+    private int[] goblinParty;
+    private int[] goblinKills;
+
+    //Constructor here
+
+    public void addGoblin(Goblin goblinToAdd){
+      //TODO
+    }
+
+    public bool canAfford(){
+        //TODO
+    }
+
+
+    //Polymorphism
+    public void pickUpCoin(Coin foundCoin){
+       //TODO
+    }
+
+    public void pickUpWeapon(Weapon foundWeapon){
+        //TODO
+    }
+
+    public void pickUpPotion(Potion foundPotion){
+        //TODO
+    }
+
+    public void giveCoinToGoblin(Goblin receivingGoblin){
+        //TODO
+
+    }
+
+    public void switchWeapon(Weapon nextWeapon){
+        //TODO
+    }
+
+    //polymorphism &&&&&&
+    public void dropWeapon(){
+        //TODO
+    }
+
+    public void dropPotion(){
+        //TODO
+    }
+
+    public void takePotion(){
+        //TODO
+    }
+
+    public void goblinKill(){
+        //TODO
+    }
+
+    public void levelUp(){
+        //TODO
+    }
+
+    public void collideWith(Environment){
+        //TODO
+    }
+
+
+}
