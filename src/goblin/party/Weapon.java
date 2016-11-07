d12538d8ad6a85ddc05b5c6d940a51f4e1525545
Robert Kendl <r_kendl@yahoo.com>
2016-11-06 17:49:34 -0700

    Added New Class Files

---
+++ src/goblin/party/Weapon.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goblin.party;
+
+/**
+ *
+ * @author
+ */
+public class Weapon extends Item{
+    private String weaponName;
+    private String weaponType;
+    private int weaponPower;
+    private int levelRequirement;
+    private int[] weaponPicture;
+    
+    public void setWeaponType(String type){
+        weaponType = type;
+        
+    }
+    
+    public void setWeaponPower(int power){
+        weaponPower = power;
+    }
+    
+    public void setLevelRequirement(int required){
+        levelRequirement = required;
+    }
+    
+    public int checkLevelRequired(){
+        return levelRequirement;    
+    }
+}
