7d98a8ce2bea61ffd4358a76718a571253a6d96b
kr0428 <kristen.hanslik@colorado.edu>
2016-11-25 19:37:15 -0700

    This code can be run in accordance with the README in the parent folder

--- src/goblin/party/GoblinPartyGame/GoblinParty.java
+++ src/goblin/party/GoblinPartyGame/GoblinParty.java
@@ -16,6 +16,7 @@ public class GoblinParty {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+	System.out.println("Starting the Goblin Party Game...");
     }
     
 }
