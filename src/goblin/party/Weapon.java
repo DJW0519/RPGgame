968ae39588dd1530d07109635edc10f43ec6cb85
John Murphy <John@econ2-209-64-dhcp.int.colorado.edu>
2016-12-03 14:25:11 -0700

    Extends Item classes

--- src/goblin/party/Weapon.java
+++ src/goblin/party/Weapon.java
@@ -15,21 +15,36 @@ public class Weapon extends Item{
     private int weaponPower;
     private int levelRequirement;
     private int[] weaponPicture;
-    
+
+    //setters
     public void setWeaponType(String type){
         weaponType = type;
-        
     }
-    
+    public void setWeaponName(String name){
+        weaponName = name;
+    }
     public void setWeaponPower(int power){
         weaponPower = power;
     }
-    
     public void setLevelRequirement(int required){
         levelRequirement = required;
     }
-    
+
+    //gettters
     public int checkLevelRequired(){
-        return levelRequirement;    
+        return levelRequirement;
     }
+    public String getWeaponType(){
+        return weaponType;
+    }
+    public String getWeaponName(){
+        return weaponName;
+    }
+    public int getWeaponPower(){
+        return weaponPower;
+    }
+    public int[] getweaponPicture(){
+        return weaponPicture;
+    }
+
 }
