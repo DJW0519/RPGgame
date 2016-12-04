d2b8e6c78f58e9e94f66973ddeac09985013e135
Robert Kendl <r_kendl@yahoo.com>
2016-12-04 14:36:23 -0700

    Update Character.java

--- src/goblin/party/GoblinPartyGame/Character.java
+++ src/goblin/party/GoblinPartyGame/Character.java
@@ -13,32 +13,73 @@ public abstract class Character{
 	private final int DEFAULT_HEALTH_LEVEL = 50;
 	private final int MIN_SKILL_LEVEL = 1;
     	protected String name;
-    	protected int healthLevel;
-    	protected int skillLevel;
-        protected int damagePerHit;
+        private int baseHealth;
+    	private int healthLevel;
+    	private int skillLevel;
+        private int basePower;
         private Weapon currentWeapon;
 
     	public Character(String name){
 		this.healthLevel = DEFAULT_HEALTH_LEVEL;
+                this.baseHealth = DEFAULT_HEALTH_LEVEL;
 		this.name = name;
 		this.skillLevel = MIN_SKILL_LEVEL;
+                this.currentWeapon = new Weapon(0, "Fists", 0);
 	
    	}
 
     public void characterDie(){
+    
     }
     
     public void attack(Character opponent){
     }
     
+    public void checkWeapon(){
+        System.out.println("Your current weapon is: " + currentWeapon.getItemName());
+        System.out.println("Weapon Size: " + currentWeapon.getSize());
+        System.out.println("Weapon Damage: " + currentWeapon.getWeaponPower());
+        System.out.println("Level Requirement: " + currentWeapon.checkLevelRequired());
+    }
+    public void setSkillLevel(int newSkillLevel){
+        skillLevel = newSkillLevel;
+    }
+    public void setBaseHealth(int newBaseHealth){
+        baseHealth = newBaseHealth;
+    }
+    public void setBasePower(int newPower){
+        basePower = newPower;
+    }
     public void setHealthLevel(int newHealthLevel){
-        healthLevel = newHealthLevel;
+        if (newHealthLevel > baseHealth)
+            healthLevel = baseHealth;
+        else
+            healthLevel = newHealthLevel;
     }
     
     public int getHealthLevel(){
         return healthLevel;
-
-
-
+    }
+    
+    public int getBaseHealth(){
+        return baseHealth;
+    }
+    public int getBasePower(){
+        return basePower;
+    }
+    public int getSkillLevel(){
+        return skillLevel;
+    }
+    
+     public String getWeaponName(){
+        return currentWeapon.getItemName();
+    }
+    
+    public int getWeaponPower(){
+        return currentWeapon.getWeaponPower();
+    }
+    
+    public void switchWeapon(Weapon nextWeapon){
+        currentWeapon = nextWeapon;
     }
 }
