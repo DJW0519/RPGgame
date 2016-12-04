0474545f5749a26ba366471d5def2e5d22ea5c4b
Robert Kendl <r_kendl@yahoo.com>
2016-12-04 13:52:01 -0700

    Update Weapon.java

--- src/goblin/party/GoblinPartyGame/Weapon.java
+++ src/goblin/party/GoblinPartyGame/Weapon.java
@@ -11,21 +11,31 @@ package GoblinPartyGame;
  */
 
 public class Weapon extends Item{
-
+    
+    private String weaponType;
     private int weaponPower;
     private int levelRequirement;
 
     public Weapon(){
         super(1);
     }
-    public Weapon(int size, String name, int power, int levelReq){
+    public Weapon(int size, String name, String type, int power, int levelReq){
       super(size, name);
+      weaponType = type;
       weaponPower = power;
       levelRequirement = levelReq;
+    }
+    public Weapon(int size, String name, int power){
+        super(size, name);
+        weaponPower = power;
+        levelRequirement = 0;
     }
 
 
     //setters
+    public void setWeaponType(String type){
+        weaponType = type;
+    }
     public void setWeaponPower(int power){
         weaponPower = power;
     }
@@ -37,8 +47,10 @@ public class Weapon extends Item{
     public int checkLevelRequired(){
         return levelRequirement;
     }
+    public String getWeaponType(){
+        return weaponType;
+    }
     public int getWeaponPower(){
         return weaponPower;
     }
-
 }
