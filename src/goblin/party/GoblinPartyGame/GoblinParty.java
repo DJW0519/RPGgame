06b2e8bd1783cfaacec78aa9284ba65fead3565f
johnmurph27 <jomu8186@colorado.edu>
2016-12-03 17:40:00 -0700

    User input

--- src/goblin/party/GoblinPartyGame/GoblinParty.java
+++ src/goblin/party/GoblinPartyGame/GoblinParty.java
@@ -15,17 +15,25 @@ public class GoblinParty {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	System.out.println("Starting the Goblin Party Game...");
-	
-	//Testing out functions and classes
-	Player testPlayer = new Player("outfitColor", "HColor","Name");
-	GoblinCoin testGoblinCoin = new GoblinCoin(20);
-	if(testPlayer.pickUpItem(testGoblinCoin) == 0){
-		System.out.println("Add was successful");
-	}
-	else{
-		System.out.println("Add was UNsuccessful");
-	}
+      //user input
+      Scanner keyboard = New Scanner(System.in);
+
+    	System.out.println("Starting the Goblin Party Game...");
+
+    	//Testing out functions and classes
+    	Player testPlayer = new Player("outfitColor", "HColor","Name");
+    	GoblinCoin testGoblinCoin = new GoblinCoin(20);
+      Weapon axe = new Weapon(2, "Axe", 5, 1);
+      if (testPlayer.pickUpItem(axe) == 0) {
+        System.out.println("Weapon added");
+
+      }
+    	if(testPlayer.pickUpItem(testGoblinCoin) == 0){
+    		System.out.println("Add was successful");
+    	}
+    	else{
+    		System.out.println("Add was UNsuccessful");
+    	}
     }
-    
+
 }
