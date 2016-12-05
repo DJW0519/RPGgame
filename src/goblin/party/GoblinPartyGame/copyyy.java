2791f0cbc94ca258f678cf746c76752c64304f8f
kr0428 <kristen.hanslik@colorado.edu>
2016-12-04 17:55:54 -0700

    Adding GoEast function to pick up coin

---
+++ src/goblin/party/GoblinPartyGame/copyyy.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GoblinPartyGame;
+import java.util.*;
+import java.lang.*;
+
+/**
+ *
+ * @author Kristen Hanslik, Robert Kendl, John Murphy, Darren White
+ */
+public class GameHandler {
+
+    /**
+     * @param args the command line arguments
+     */
+
+
+    public void goSouth(Player thePlayer){
+      Scanner keyboard = new Scanner(System.in);
+	String answer;
+	Goblin friendlyGoblin = new Goblin("Robby", "Good", "Blue", 5);
+	System.out.println("You encounter a friendly " + friendlyGoblin.getColor() + " Goblin!");
+	System.out.print("Would you like to hire it for ");
+	System.out.println(friendlyGoblin.getCharge() + " " + friendlyGoblin.getColor() + " coins? (y/n)");
+	answer = keyboard.nextLine();
+	if(answer.equals("y")){
+		if(thePlayer.payGoblin(friendlyGoblin.getCharge(), friendlyGoblin.getColor()) == 0){
+			System.out.println("Success! The Goblin has been added to your party!");
+		}
+		else{
+			System.out.println("You do not have enough " + friendlyGoblin.getColor() + " coins!");
+		}
+	}
+	while(true){
+		System.out.println("Looks like there is nothing else to do here.");
+		System.out.println("1: Go back to center");
+		System.out.println("2: Stay here");
+		answer = keyboard.nextLine();
+		if(answer.equals("1")){
+			return;
+		}
+	}
+    }
+    public void goNorth(Player thePlayer){
+	Scanner keyboard = new Scanner(System.in);
+        String answer;
+        
+	System.out.println("You stumble upon a large beefcake of a goblin. "
+                + "Around his neck is a collar with a pendant reading 'Billy'. "
+                + "He looks like he's ready for a fight. What do you do?:");
+        System.out.println("1: Fight him");
+        System.out.println("2: Run!");
+        answer = keyboard.nextLine();
+        if(answer.equals("1")){
+            Goblin billy = new Goblin("Billy", "Goblin Body Builder", "Green", 10);
+            int playerHealth = thePlayer.getHealthLevel();
+            int billyHealth = billy.getHealthLevel();
+            System.out.println("You choose to give the goblin the fight he desires. He squats down and "
+            + "flexes his massive, veiny biceps. You ready your weapon.");
+            while (playerHealth > 0){
+                System.out.println("\nGoblin's Health: " + billyHealth);
+                System.out.println("Your Health: " + playerHealth);
+                System.out.println("\nWhat do you do?:" + "\n 1: Attack \n 2: Use Potion \n 3: Run");
+                answer = keyboard.nextLine();
+                switch (answer){
+                    case "1": 
+                            System.out.println("You attack Billy with your " + thePlayer.getWeaponName());
+                            System.out.println("Base Power: " + thePlayer.getBasePower());
+                            System.out.println("Weapon Power: " + thePlayer.getWeaponPower());
+                            System.out.println("Party Power: " + thePlayer.getPartySize()* 2 + " (2 power per Goblin)");
+                            int playerDamageDone = 
+                            thePlayer.getBasePower() + thePlayer.getWeaponPower() + (thePlayer.getPartySize()* 2) ; 
+                            System.out.println("You do " + playerDamageDone + " damage");
+                            billyHealth -= playerDamageDone;
+                            billy.setHealthLevel(billyHealth);
+                            try {
+                            Thread.sleep(2000);                 
+                            } catch(InterruptedException ex) {
+                            Thread.currentThread().interrupt();
+                            }
+                            System.out.println("\nThe goblin hits you with a massive punch. You lose 10 health \n");
+                            playerHealth -= 10;
+                            thePlayer.setHealthLevel(playerHealth);
+                            try {
+                            Thread.sleep(1000);                 
+                            } catch(InterruptedException ex) {
+                            Thread.currentThread().interrupt();
+                            }
+                            if(billyHealth <= 0){
+                                billyHealth = 0;
+                                System.out.println("\n You triumph over the massive goblin with one last blow. He falls over dead.\n");
+                                thePlayer.levelUp();
+                            }
+                            break;
+                            
+                    case "2": 
+                            System.out.println("potion");
+                            break;
+                          
+                    case "3":
+                            System.out.println("\nYou chicken out. His massive, sculpted body is " +
+                            "way too much for you to handle. You go back to where you were before.\n");
+                            billyHealth = 0;
+                            break;
+                            
+                    default: 
+                            System.out.println("\nYou just sit there and do nothing, staring blankly out into space."
+                            + "\nThe goblin waits patiently for a few minutes before it gets bored and punches you HARD.\nYou lose 10 health\n");
+                            playerHealth -= 10;
+                            thePlayer.setHealthLevel(playerHealth);
+                            break;
+                }
+                if (playerHealth == 0){
+                    System.out.println("\nThe goblin's last punch slams into your face one last time."
+                            + " His rippling muscles have enough force to pierce through your head.\n");
+                    System.out.println("YOU ARE DEAD!!! \n");
+                }
+                if(billyHealth == 0)
+                    break;
+            }
+        }
+        else if (answer.equals("0")){
+            System.out.println("You chicken out. His massive, sculpted body is " +
+                    "way too much for you to handle. You go back to where you were before.");
+        }
+        else{
+            System.out.println("You just sit there and do nothing, staring blankly out into space."
+                    + "The goblin waits patiently for a few minutes before it gets bored and punches you HARD.");
+            System.out.println("His punch is so strong that it knocks you back...in time.");
+        }    
+	}
+
+    public void goWest(Player thePlayer){
+       Scanner keyboard = new Scanner(System.in);
+      Random randNum = new Random();
+      Weapon axe = new Weapon(15, "Axe", 8, 1);
+      Weapon sword = new Weapon(10, "Sword", 5, 1);
+      Weapon mace = new Weapon(12, "Mace", 6, 1);
+      ArrayList<Item> weapons;
+      weapons = new ArrayList<Item>();
+      weapons.add(axe);
+      weapons.add(sword);
+      weapons.add(mace);
+      int currentWeapon;
+	    String answer;
+	    int randomInt = randNum.nextInt(3);
+	    System.out.println("You look down and see a " + weapons.get(randomInt).name + " just below the surface. Do you grab it?");
+	    answer = keyboard.nextLine();
+
+	    if(answer.equals("y")){
+	      if(thePlayer.pickUpItem(weapons.get(randomInt)) == 0){
+		System.out.println("You are now holding the " + weapons.get(randomInt).name);
+		currentWeapon = randomInt;
+	      }
+	      else System.out.println("You do not have space for this weapon");
+	    }
+
+	while(true){
+		System.out.println("Looks like there is nothing else to do here.");
+		System.out.println("1: Go back to center");
+		System.out.println("2: Stay here");
+		answer = keyboard.nextLine();
+		if(answer.equals("1")){
+			return;
+		}
+    	}
+}	
+}
